7b6b37f2a9bafac9a6900de244972d32b0ff9e3d
toandaominh1997 <toandaominh1997@gmail.com>
2019-05-05 18:25:08 +0700

    Add vim config

---
+++ install.sh
@@ -0,0 +1,9 @@
+set -e
+mkdir ~/.dotfiles
+cd ~/.dotfiles
+
+source ~/.dotfiles/vimrcs/basic.vim
+source ~/.dotfiles/vimrcs/plugin.vim
+> ~/.vimrcs
+
+echo "Installed Vim configuration successfully ^~^"
