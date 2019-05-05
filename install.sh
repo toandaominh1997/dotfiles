d01f3aff38537d0788c80ed3b8d1507fad1821e8
toandaominh1997 <toandaominh1997@gmail.com>
2019-05-06 01:15:49 +0700

    update

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
 set +e
 set -u
 
-if [ -d ~/.dotfiles ] ; then
-cd ~/.dotfiles
+if [ -d ~/.toandaominh1997/.dotfiles ] ; then
+cd ~/.toandaominh1997/.dotfiles
 git pull origin master
 fi
 
 echo '
-set runtimepath+=~/.dotfiles
-source ~/.dotfiles/vimrcs/plugin.vim
-source ~/.dotfiles/vimrcs/basic.vim
+set runtimepath+=~/.toandaominh1997/.dotfiles
+source ~/.toandaominh1997/.dotfiles/vimrcs/plugin.vim
+source ~/.toandaominh1997/.dotfiles/vimrcs/basic.vim
 '> ~/.vimrc
 
-if [ ! -d ~/.vim/bundle ] ; then
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.dotfiles/bundle/Vundle.vim
+if [ ! -d ~/.toandaominh1997/bundle ] ; then
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.toandaominh1997/bundle/Vundle.vim
 fi
 
 vim -E +PluginInstall +qall
-if [ -d ~/.vim/bundle/youcompleteme ] ; then
+if [ -d ~/.toandaominh1997/bundle/youcompleteme ] ; then
 echo 'Install youcompleteme'
-cd ~/.vim/bundle/youcompleteme
+cd ~/.toandaominh1997/bundle/youcompleteme
 python3 install.py --all
 fi
 
@@ -29,27 +29,27 @@ if ! hash zsh; then
 sudo apt-get install -y zsh
 fi
 cd 
-if [ ! -d ~/.oh-my-zsh ] ; then
+if [ ! -d ~/.toandaominh1997/.oh-my-zsh ] ; then
 echo 'Install Oh my zsh'
 # sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.dotfiles/.oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.toandaominh1997/.oh-my-zsh
 fi
 
 cd
-if [ ! -d ~/.dotfiles/fonts ] ; then
-git clone https://github.com/powerline/fonts.git ~/.dotfiles/fonts
-sh ~/.dotfiles/fonts/install.sh
+if [ ! -d ~/.toandaominh1997/.dotfiles/fonts ] ; then
+git clone https://github.com/powerline/fonts.git ~/.toandaominh1997/.dotfiles/fonts
+sh ~/.toandaominh1997/.dotfiles/fonts/install.sh
 fi
 
 
 cd
-if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ] ; then
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/dotfiles/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ ! -d ~/.toandaominh1997/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ] ; then
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.toandaominh1997/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 fi
 
 cd
-if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ] ; then
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.dotfiles/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [ ! -d ~/~/.toandaominh1997/.oh-my-zsh/custom/plugins/zsh-autosuggestions ] ; then
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.toandaominh1997/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 fi
 
 echo '
