72ba23ce1710313f85bbaf2da526447cd6158628
toandaominh1997 <toandaominh1997@gmail.com>
2019-05-05 19:14:49 +0700

    update

---
+++ gitpush.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m"update"
+git push origin master -toandaominh1997 -Optimize4life1997@
--- install.sh
+++ install.sh
@@ -1,17 +1,26 @@
 set -e
+
+if [ -d ~/.dotfiles ] ; then
 cd ~/.dotfiles
+git pull origin master
+fi
 
 echo '
 set runtimepath+=~/.dotfiles
 source ~/.dotfiles/vimrcs/plugin.vim
 source ~/.dotfiles/vimrcs/basic.vim
 '> ~/.vimrc
-ls
-rm -rf ~/.vim/bundle
+if [ -d ~/.vim/bundle ] ; then
+echo 'vim bundle exist'
+else #if needed #also: elif [new condition] 
+echo 'clone vim bundle'
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim -E +VimEnter +PluginInstall +qall
+fi
 
-cd ~/.vim/bundle/YouCompleteMe
+# vim +VimEnter +PluginInstall +qall
+if [ -d ~/.vim/bundle/youcompleteme ] ; then
+echo 'Install youcompleteme'
+cd ~/.vim/bundle/youcompleteme
 python3 install.py --all
-
+fi
 echo "Installed Vim configuration successfully ^~^"
