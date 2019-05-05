ed8a615ff8d24526844fe8671dcd9903fc34c9f8
toandaominh1997 <toandaominh1997@gmail.com>
2019-05-05 19:00:40 +0700

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 set -e
 cd ~/.dotfiles
 
-source ~/.dotfiles/vimrcs/basic.vim
+echo '
+set runtimepath+=~/.dotfiles
 source ~/.dotfiles/vimrcs/plugin.vim
-> ~/.vimrcs
+source ~/.dotfiles/vimrcs/basic.vim
+'> ~/.vimrc
+ls
+rm -rf ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -E +VimEnter +PluginInstall +qall
+
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --all
 
 echo "Installed Vim configuration successfully ^~^"
--- vimrcs/plugin.vim
+++ vimrcs/plugin.vim
@@ -8,11 +8,12 @@ Plugin 'VundleVim/Vundle.vim'
 Plugin 'vim-airline/vim-airline'
 Plugin 'vim-airline/vim-airline-themes'
 Plugin 'majutsushi/tagbar'
+Plugin 'airblade/vim-gitgutter'
+Plugin 'valloric/youcompleteme'
 Plugin 'dracula/vim'
  
 call vundle#end()
 filetype plugin indent on
- 
 syntax on
  
 color dracula
