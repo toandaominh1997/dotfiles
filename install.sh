6f544c9e17ba005f97b113f8a3f0331b304f6d36
toandaominh1997 <toandaominh1997@gmail.com>
2019-05-06 15:16:54 +0700

    update

--- install.sh
+++ install.sh
@@ -54,32 +54,6 @@ fi
 echo '
 source ~/.toandaominh1997/.dotfiles/tmuxs/tmux.conf
 '> ~/.tmux.conf
-# if [ ! -e "$HOME/.tmux/plugins/tpm" ]; then
-#   printf "WARNING: Cannot found TPM (Tmux Plugin Manager) \
-#  at default location: \$HOME/.tmux/plugins/tpm.\n"
-#   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# fi
-
-# if [ -e "$HOME/.tmux.conf" ]; then
-#   printf "Found existing .tmux.conf in your \$HOME directory. Will create a backup at $HOME/.tmux.conf.bak\n"
-# fi
-
-# cp -f "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak" 2>/dev/null || true
-# cp -a ./tmuxs/. "$HOME"/.tmux/
-# ln -sf .tmux/tmux.conf "$HOME"/.tmux.conf;
-
-# # Install TPM plugins.
-# # TPM requires running tmux server, as soon as `tmux start-server` does not work
-# # create dump __noop session in detached mode, and kill it when plugins are installed
-# printf "Install TPM plugins\n"
-# tmux new -d -s __noop >/dev/null 2>&1 || true 
-# tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "~/.tmux/plugins"
-# "$HOME"/.tmux/plugins/tpm/bin/install_plugins || true
-# tmux kill-session -t __noop >/dev/null 2>&1 || true
-
-# printf "OK: Completed\n"
-
-
 
 if ! hash zsh; then
 apt-get install -y zsh
