f5b1c2594cc1e16af6eed6fb21abbb22ae842218
toandaominh1997 <toandaominh1997@gmail.com>
2020-05-05 00:46:09 +0700

    Update

--- snap.sh
+++ snap.sh
@@ -1,3 +1,60 @@
+command_exists() {
+    hash "$1" &>/dev/null
+}
+# install git
+if command_exists git; then
+    echo "git is installed"
+else
+    echo "WARNING: \"git\" command is not found. Install it first"
+    apt-get install -y git
+fi
+
+# install curl
+if command_exists curl; then
+    echo "curl is installed"
+else
+    echo "WARNING: \"curl\" command is not found. Install it first"
+    apt-get install -y curl
+fi
+
+# install wget
+if command_exists wget; then
+    echo "wget is installed"
+else
+    echo "require wget but it's not installed. Install it first"
+    apt-get install -y wget
+fi
+
+# install vim stable
+if command_exists vim; then
+    echo "vim is installed"
+else
+    echo "require vim but it's not installed. Install it first"
+    add-apt-repository ppa:jonathonf/vim
+    apt-get update
+    apt-get install -y vim
+fi
+
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install zsh stable
+if command_exists zsh; then
+    echo "zsh is installed"
+else
+    echo "require zsh but it's not installed. Install it first"
+    apt-get install -y zsh
+fi
+
+# install tmux stable
+if command_exists tmux; then
+    echo "tmux is installed"
+else
+    echo "require tmux but it's not installed. Install it first"
+    apt-get install -y tmux
+fi
+
 # install nodejs
 snap install node --channel=14/stable --classic
 # install nvim
