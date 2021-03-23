f25fba167f8847af6813dbc1da478f4881db7889
Doan Minh Toan <toandm2@VID-TOANDM2-L.local>
2021-03-23 18:34:40 +0700

    Support brew on macos

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@ command_exists() {
 # install git
 if command_exists git; then
     echo "git is installed"
-else
+elif [ "$(uname)" == "Darwin" ]; then
+    echo "WARNING: \"git\" command is not found. Install it first"
+    brew install -y git
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "WARNING: \"git\" command is not found. Install it first"
     apt-get install -y git
 fi
@@ -12,7 +15,10 @@ fi
 # install curl
 if command_exists curl; then
     echo "curl is installed"
-else
+elif [ "$(uname)" == "Darwin" ]; then
+    echo "WARNING: \"curl\" command is not found. Install it first"
+    brew install -y curl
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "WARNING: \"curl\" command is not found. Install it first"
     apt-get install -y curl
 fi
@@ -20,24 +26,34 @@ fi
 # install wget
 if command_exists wget; then
     echo "wget is installed"
-else
-    echo "require wget but it's not installed. Install it first"
+elif [ "$(uname)" == "Darwin" ]; then
+    echo "WARNING: \"wget\" command is not found. Install it first"
+    brew install -y wget
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "WARNING: \"wgetl\" command is not found. Install it first"
     apt-get install -y wget
 fi
 
 # install vim stable
 if command_exists vim; then
     echo "vim is installed"
-else
+elif [ "$(uname)" == "Darwin" ]; then
+    echo "WARNING: \"vim\" command is not found. Install it first"
+    brew install -y vim
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "require vim but it's not installed. Install it first"
     add-apt-repository -y ppa:jonathonf/vim
     apt-get update -y
     apt-get install -y vim
     apt-get install -y vim-gnome
 fi
+
 if command_exists nvim; then
     echo "nvim is installed"
-else
+elif [ "$(uname)" == "Darwin" ]; then
+    echo "WARNING: \"wget\" command is not found. Install it first"
+    brew install -y neovim
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "require nvim but it's not installed. Install it first"
     add-apt-repository -y ppa:neovim-ppa/stable
     apt-get update -y
@@ -53,16 +69,22 @@ echo "vim-plug installed"
 # install zsh stable
 if command_exists zsh; then
     echo "zsh is installed"
-else
-    echo "require zsh but it's not installed. Install it first"
+elif [ "$(uname)" == "Darwin" ]; then
+    echo "WARNING: \"zsh\" command is not found. Install it first"
+    brew install -y zsh
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "WARNING: \"zsh\" command is not found. Install it first"
     apt-get install -y zsh
 fi
 
 # install tmux stable
 if command_exists tmux; then
     echo "tmux is installed"
-else
-    echo "require tmux but it's not installed. Install it first"
+elif [ "$(uname)" == "Darwin" ]; then
+    echo "WARNING: \"zsh\" command is not found. Install it first"
+    brew install -y tmux
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "WARNING: \"tmux\" command is not found. Install it first"
     apt-get install -y tmux
 fi
 
