0ad2b770c0ebea057b4c3ee98d8f89c984d23a1a
toandaominh1997 <toandaominh1997@gmail.com>
2021-10-10 22:53:06 +0700

    Change apt to brew

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 command_exists() {
     hash "$1" &>/dev/null
 }
+# install brew
+if command_exists git; then
+    echo "brew is installed"
+elif [ "$(uname)" == "Darwin" ]; then
+    echo "WARNING: \"brew\" command is not found. Install it first"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "WARNING: \"brew\" command is not found. Install it first"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    sudo ln -sfn /home/linuxbrew/.linuxbrew/bin/brew /usr/local/bin/brew
+fi
+
+
 
 if [ ! -d $HOME/.dotfiles/development ]; then
     echo "Neovim setup"
@@ -9,64 +22,41 @@ fi
 # install git
 if command_exists git; then
     echo "git is installed"
-elif [ "$(uname)" == "Darwin" ]; then
+elif [[ "$(uname)" == "Darwin" || "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
     echo "WARNING: \"git\" command is not found. Install it first"
     brew install git
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "WARNING: \"git\" command is not found. Install it first"
-    apt-get install -y git
-fi
+fi 
 
 # install curl
 if command_exists curl; then
     echo "curl is installed"
-elif [ "$(uname)" == "Darwin" ]; then
+elif [[ "$(uname)" == "Darwin" || "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
     echo "WARNING: \"curl\" command is not found. Install it first"
     brew install curl
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "WARNING: \"curl\" command is not found. Install it first"
-    apt-get install -y curl
-fi
+fi 
 
 # install wget
 if command_exists wget; then
     echo "wget is installed"
-elif [ "$(uname)" == "Darwin" ]; then
-    echo "WARNING: \"wget\" command is not found. Install it first"
+elif [[ "$(uname)" == "Darwin" || "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
+    echo "WARNING: \"curl\" command is not found. Install it first"
     brew install wget
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "WARNING: \"wgetl\" command is not found. Install it first"
-    apt-get install -y wget
-fi
+fi 
 
 # install vim stable
 if command_exists vim; then
     echo "vim is installed"
-elif [ "$(uname)" == "Darwin" ]; then
+elif [[ "$(uname)" == "Darwin" || "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
     echo "WARNING: \"vim\" command is not found. Install it first"
     brew install vim
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "require vim but it's not installed. Install it first"
-    add-apt-repository -y ppa:jonathonf/vim
-    apt-get update -y
-    apt-get install -y vim
-    apt-get install -y vim-gnome
-fi
+fi 
 
 if command_exists nvim; then
     echo "nvim is installed"
-elif [ "$(uname)" == "Darwin" ]; then
+elif [[ "$(uname)" == "Darwin" || "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
     echo "WARNING: \"nvim\" command is not found. Install it first"
     brew install neovim
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "require nvim but it's not installed. Install it first"
-    wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O $HOME/.dotfiles/development/nvim.appimage
-    chmod u+x $HOME/.dotfiles/development/nvim.appimage
-    sudo ln -sfn $HOME/.dotfiles/development/nvim.appimage /usr/local/bin/nvim
-    #add-apt-repository -y ppa:neovim-ppa/stable
-    #apt-get update -y
-    #apt-get install -y neovim
-fi
+fi 
 
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -77,37 +67,23 @@ echo "vim-plug installed"
 # install zsh stable
 if command_exists zsh; then
     echo "zsh is installed"
-elif [ "$(uname)" == "Darwin" ]; then
+elif [[ "$(uname)" == "Darwin" || "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
     echo "WARNING: \"zsh\" command is not found. Install it first"
     brew install zsh
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "WARNING: \"zsh\" command is not found. Install it first"
-    apt-get install -y zsh
-fi
+fi 
 
 # install tmux stable
 if command_exists tmux; then
     echo "tmux is installed"
-elif [ "$(uname)" == "Darwin" ]; then
+elif [[ "$(uname)" == "Darwin" || "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
     echo "WARNING: \"tmux\" command is not found. Install it first"
     brew install tmux
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "WARNING: \"tmux\" command is not found. Install it first"
-    wget https://github.com/tmux/tmux/releases/download/3.2a/tmux-3.2a.tar.gz -O $HOME/.dotfiles/development/tmux-3.2a.tar.gz
-    tar xvzf $HOME/.dotfiles/development/tmux-3.2a.tar.gz -C $HOME/.dotfiles/development
-    cd $HOME/.dotfiles/development/tmux-3.2a
-    sudo apt install -y libevent-dev
-    ./configure && make
-    sudo ln -sfn $HOME/.dotfiles/development/tmux-3.2a/tmux /usr/local/bin/tmux
-    #apt-get install -y tmux
-fi
+fi 
 
 # install node lts
 if command_exists node; then
     echo "nodejs is installed"
-else
-    echo "require nodejs but it's not installed. Install it first"
-    wget install-node.now.sh/lts
-    bash lts
-    rm -rf lts
-fi
+elif [[ "$(uname)" == "Darwin" || "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
+    echo "WARNING: \"node\" command is not found. Install it first"
+    brew install node
+fi 
