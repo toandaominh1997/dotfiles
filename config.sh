52e727a146cff97019df42dc5cb0d0058a182324
toandaominh1997 <toandaominh1997@gmail.com>
2022-04-23 01:18:42 +0700

    Refactor lua file

--- config.sh
+++ config.sh
@@ -9,6 +9,7 @@ if [[ ! -d $HOME/.config/nvim ]]; then
     mkdir $HOME/.config/nvim
 fi
 echo -e "set runtimepath^=~/.vim runtimepath+=~/.vim/after\nlet &packpath = &runtimepath\nsource $HOME/.dotfiles/tool/vim/config.vim" >$HOME/.config/nvim/init.vim
+ln -s ~/.dotfiles/tool/vim/lua ~/.config/nvim/lua
 
 #vim +PlugInstall +qall
 #nvim +PlugInstall +qall
---
+++ vim/lua/user/treesitter.lua
@@ -0,0 +1,25 @@
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = {
+    "python",
+    "json",
+    "yaml",
+    "javascript",
+    "typescript",
+    "go"
+    }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+  ignore_install = {}, -- List of parsers to ignore installing
+  highlight = {
+    enable = false,              -- false will disable the whole extension
+    disable = {},  -- list of language that will be disabled
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = false,
+    disable = {},
+  },
+}
