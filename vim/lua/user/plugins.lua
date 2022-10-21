fff6d5af5113bb82c524c2a42aad38415fe5e9fa
toandaominh1997 <toandaominh1997@gmail.com>
2022-10-21 08:14:53 +0700

    Add tmux navigator

--- vim/lua/user/plugins.lua
+++ vim/lua/user/plugins.lua
@@ -45,6 +45,8 @@ return packer.startup(function(use)
 
   use { "kyazdani42/nvim-web-devicons" }
 
+  use { "christoomey/vim-tmux-navigator" }
+
   -- Colorschemes
   use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
   use { "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" }
