c8f6efa8e760fb8a68f9b29ad29ee6a7d4bf70af
toandaominh1997 <toandaominh1997@gmail.com>
2022-10-31 19:22:08 +0700

    Add indentline

--- vim/lua/user/indentline.lua
+++ vim/lua/user/indentline.lua
@@ -58,7 +58,7 @@ vim.wo.colorcolumn = "99999"
 -- vim.opt.listchars:append "eol:↴"
 
 indent_blankline.setup({
-	-- show_end_of_line = true,
+	show_end_of_line = true,
 	-- space_char_blankline = " ",
 	show_current_context = true,
 	-- show_current_context_start = true,
