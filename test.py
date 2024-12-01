dd955b6ff87740e0a04e5c9b800bafbd2109b0a0
Henry <toandaominh1997@gmail.com>
2024-12-02 02:07:32 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from typing import List, Union, Generator, Iterator
 import os 
-
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
 
 class Pipeline:
     def __init__(self):
@@ -8,6 +9,15 @@ class Pipeline:
 
     async def on_startup(self):
         # This function is called when the server is started.
+        template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+        prompt = ChatPromptTemplate.from_template(template)
+        
+        model = OllamaLLM(model="llama3.2")
+        
+        self.chain = prompt | model
         pass
 
     async def on_shutdown(self):
@@ -18,4 +28,5 @@ class Pipeline:
         self, user_message: str, model_id: str, messages: List[dict], body: dict
     ) -> Union[str, Generator, Iterator]:
         self.index += 1
-        return f"Index at {self.index}"
+        answer = chain.invoke({"question": user_message})
+        return answer
