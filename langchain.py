0fafbc7ba0dfd55b21d9dd20a47dc8f9a196b4f5
Henry <toandaominh1997@gmail.com>
2024-12-02 02:32:00 +0700

    Update langchain.py

--- langchain.py
+++ langchain.py
@@ -9,15 +9,7 @@ class Pipeline:
 
     async def on_startup(self):
         # This function is called when the server is started.
-        template = """Question: {question}
 
-Answer: Let's think step by step."""
-
-        prompt = ChatPromptTemplate.from_template(template)
-        
-        model = OllamaLLM(model="llama3.2")
-        
-        self.chain = prompt | model
         pass
 
     async def on_shutdown(self):
@@ -28,5 +20,14 @@ Answer: Let's think step by step."""
         self, user_message: str, model_id: str, messages: List[dict], body: dict
     ) -> Union[str, Generator, Iterator]:
         self.index += 1
-        answer = self.chain.invoke({"question": user_message})
+        template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+        prompt = ChatPromptTemplate.from_template(template)
+        
+        model = OllamaLLM(model="llama3.1")
+        
+        chain = prompt | model
+        answer = chain.invoke({"question": user_message})
         return answer
